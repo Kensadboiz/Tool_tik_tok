efe7b879bf347f0a2654e5830972905ccffd0233
Kensadboiz <twogamevn@gmail.com>
2025-04-09 07:09:32 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function downloadVideo() {
-  const link = document.getElementById('tiktok-link').value;
-  if (!link) {
-    alert("Vui lòng nhập link TikTok!");
-    return;
-  }
-  alert("Đã gửi yêu cầu tải video từ: " + link);
-  // Gọi API backend tại đây sau khi có server
-}
